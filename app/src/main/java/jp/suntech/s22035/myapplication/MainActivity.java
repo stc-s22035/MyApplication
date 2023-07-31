a0f6ef83d7f2cf9ee0f103135c240cda33dff567
stc-s22035 <s22035@suntech.jp>
2023-07-31 16:16:52 +0900

    Initial commit

---
+++ app/src/main/java/jp/suntech/s22035/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.suntech.s22035.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
